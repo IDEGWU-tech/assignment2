eb4ee617168b1a43f05c1bbb2349b971a0927553
IDEGWU-tech <udomeonyesomidegwu@gmail.com>
2025-10-05 01:57:57 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+import react from "react";
+import ReactDom from "react-dom/client";
+import app from "./app";
+const root = ReactDom.createRoot(document.getElementById("personal"));
+root.render(<app/>);
